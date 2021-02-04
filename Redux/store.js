9427b08ad5aed3d1984a5c71f095726907d54b1b
Russell Hart <russellbot@gmail.com>
2021-02-04 09:21:41 -0500

    cartItems reducer

--- Redux/store.js
+++ Redux/store.js
@@ -2,8 +2,10 @@ import { createStore, combineReducers, applyMiddleware } from 'redux';
 import thunkMiddleware from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension/developmentOnly';
 
+import cartItems from './Reducers/cartItem'
+
 const reducers = combineReducers({
-    // cartReducer
+    cartItems: cartItems
 })
 
 const store = createStore(
