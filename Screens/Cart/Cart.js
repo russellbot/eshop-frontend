b1e1274feff04b31f9d9c50b2cb21646e0ab668c
Russell Hart <russellbot@gmail.com>
2021-02-04 11:36:52 -0500

    Cart Navigation

---
+++ Screens/Cart/Cart.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Text } from 'react-native';
+
+const Cart = () => {
+    return (
+        <Text>CART WORKS</Text>
+    )
+}
+
+export default Cart;
---
+++ Screens/Cart/Checkout.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Text } from 'react-native';
+
+const Checkout = () => {
+    return (
+        <Text>CHECKOUT WORKS</Text>
+    )
+}
+
+export default Checkout;
